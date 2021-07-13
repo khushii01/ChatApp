15248ea58790e1f609b7fee9ac4b4ffc43aa741f
Khushi Singh <skhushii001@gmail.com>
2021-07-13 22:44:10 +0530

    added chat

--- utils/messages.js
+++ utils/messages.js
@@ -1,10 +1,13 @@
 const moment = require('moment');
 
+Date.prototype.timeNow = function () { return ((this.getHours() < 10) ? "0" : "") + ((this.getHours() > 12) ? (this.getHours() - 12) : this.getHours()) + ":" + ((this.getMinutes() < 10) ? "0" : "") + this.getMinutes() + ((this.getHours() > 12) ? (' PM') : ' AM'); };
+var newDate = new Date();
+var time = newDate.timeNow();
 function formatMessage(username, text) {
   return {
     username,
     text,
-    time: moment().format('h:mm a')
+    time
   };
 }
 
